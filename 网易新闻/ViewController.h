4e422afae4cc3e1b73d6d897725f41caf9c091d7
yosemite <yosemite@yosemitedeMacBook-Pro.local>
2015-01-24 22:45:47 +0000

    Initial Commit

---
+++ 网易新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网易新闻
+//
+//  Created by yosemite on 15/1/24.
+//  Copyright (c) 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 网易新闻/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网易新闻
+//
+//  Created by yosemite on 15/1/24.
+//  Copyright (c) 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
